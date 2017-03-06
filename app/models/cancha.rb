ab8d431bcaf0b1e3ceec90def5516c0b6002cde3
war1832 <war.v8sf@gmail.com>
2017-03-06 14:54:43 -0300

    se agrego el collection_select en las vistas para poder seleccionar un cliente y una cancha cuando se realiza la reserva

--- app/models/cancha.rb
+++ app/models/cancha.rb
@@ -1,4 +1,8 @@
 class Cancha < ActiveRecord::Base
   belongs_to :TipoCancha
   has_many :reservas
+  
+  def descripcion_con_tipo
+    "#{self.TipoCancha.descripcion} - #{name}"
+  end
 end
--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,4 +1,8 @@
 class Cliente < ActiveRecord::Base
     belongs_to :Tipo_cliente
     has_many :Reservas
+    
+    def nombre_y_apellido
+      "#{apellido}, #{nombre}"
+    end
 end
