76d4c67560d46a8aaeea328104a8fcb837d559dd
war1832 <war.v8sf@gmail.com>
2017-03-09 13:39:15 -0300

    se fixearon las validaciones de los turnos fijos y las reservas

--- app/models/reserva.rb
+++ app/models/reserva.rb
@@ -5,6 +5,7 @@ class Reserva < ActiveRecord::Base
   validates_presence_of :fecha_inicio
   validates_presence_of :fecha_fin
   
+  validate :validar_horario
   validate :validar_reserva
   
   def validar_reserva
@@ -13,25 +14,31 @@ class Reserva < ActiveRecord::Base
       end
   end
   
- def verificar_disponibilidad_reservas
-  Reserva.where("(fecha_inicio BETWEEN ? AND ? OR 
-                        fecha_fin BETWEEN ? AND ? ) AND  
-                        cancha_id = ? AND 
-                        fecha_baja IS NULL AND
-                        cliente_id <> ? ", (fecha_inicio + 1.seconds), (fecha_fin - 1.seconds),
-                                           (fecha_inicio + 1.seconds),(fecha_fin - 1.seconds),
-                                            cancha.id, cliente.id  ).present?
+  def verificar_disponibilidad_reservas
+    Reserva.where("(fecha_inicio BETWEEN ? AND ? OR 
+                    fecha_fin BETWEEN ? AND ? ) AND  
+                    cancha_id = ? AND 
+                    fecha_baja IS NULL AND
+                    cliente_id <> ? ", (fecha_inicio + 1.seconds), (fecha_fin - 1.seconds),
+                                       (fecha_inicio + 1.seconds),(fecha_fin - 1.seconds),
+                                        cancha.id, cliente.id  ).present?
  end
  
- def verificar_disponibilidad_turnos_fijos
-     TurnosFijo.where("(HOUR(hora_inicio) BETWEEN ? AND ? OR 
-                        HOUR(hora_fin) BETWEEN ? AND ? ) AND 
-                        dia_semana = ? AND  
-                        cancha_id = ? AND 
-                        fecha_baja IS NULL AND
-                        cliente_id <> ? ", (fecha_inicio + 1.seconds).strftime("%H:%M"), (fecha_fin - 1.seconds).strftime("%H:%M"),
-                                           (fecha_inicio + 1.seconds).strftime("%H:%M"), (fecha_fin - 1.seconds).strftime("%H:%M"),
-                                           fecha_inicio.wday, cancha.id, cliente.id  ).present?
- end
+  def verificar_disponibilidad_turnos_fijos
+      TurnosFijo.where("(HOUR(hora_inicio) BETWEEN ? AND ? OR 
+                         HOUR(hora_fin) BETWEEN ? AND ? ) AND 
+                         dia_semana = ? AND  
+                         cancha_id = ? AND 
+                         fecha_baja IS NULL AND
+                         cliente_id <> ? ", (fecha_inicio + 1.seconds).strftime("%H:%M"), (fecha_fin - 1.seconds).strftime("%H:%M"),
+                                            (fecha_inicio + 1.seconds).strftime("%H:%M"), (fecha_fin - 1.seconds).strftime("%H:%M"),
+                                            fecha_inicio.wday, cancha.id, cliente.id  ).present?
+  end
+  
+  def validar_horario    
+    if (fecha_inicio > fecha_fin) || fecha_inicio.to_date != fecha_fin.to_date
+      errors.add(:horario_incorrecto, "Verifique los horarios.") 
+    end 
+  end
   
 end
--- app/models/turnos_fijo.rb
+++ app/models/turnos_fijo.rb
@@ -5,27 +5,30 @@ class TurnosFijo < ActiveRecord::Base
   validates_presence_of :hora_inicio
   validates_presence_of :hora_fin
   
-    validate :validar_turno_fijo
+  validate :validar_turno_fijo
+  validate :validar_horario
   
   def validar_turno_fijo
-      if verificar_disponibilidad_turnos_fijos || verificar_disponibilidad_reservas
+      if verificar_disponibilidad_turnos_fijos && verificar_disponibilidad_reservas
         errors.add(:disponibilidad, "Ya existe un Turno en ese horario, verifique los datos de la reserva.")
       end
   end
   
  def verificar_disponibilidad_reservas
-# TODO: codearlo
-#  Reserva.where("(fecha_inicio BETWEEN ? AND ? OR 
-#                        fecha_fin BETWEEN ? AND ? ) AND  
-#                        cancha_id = ? AND 
-#                        fecha_baja IS NULL AND
-#                        cliente_id <> ? ", (fecha_inicio + 1.seconds), (fecha_fin - 1.seconds),
-#                                           (fecha_inicio + 1.seconds),(fecha_fin - 1.seconds),
-#                                            cancha.id, cliente.id  ).present?
+
+  Reserva.where("fecha_inicio >= ? AND
+        WEEKDAY(fecha_inicio) = ?  AND 
+      (HOUR(fecha_inicio) BETWEEN ? AND ? OR 
+       HOUR(fecha_fin) BETWEEN ? AND ? ) AND  
+                     cancha_id = ? AND 
+                fecha_baja IS NULL AND
+                      cliente_id <> ?", DateTime.now, dia_semana ,(hora_inicio + 1.seconds).strftime("%H:%M"), (hora_fin - 1.seconds).strftime("%H:%M"),
+                                           (hora_inicio + 1.seconds).strftime("%H:%M"),(hora_fin - 1.seconds).strftime("%H:%M"),
+                                            cancha.id, cliente.id  ).present?
  end
  
- def verificar_disponibilidad_turnos_fijos
-     TurnosFijo.where("(HOUR(hora_inicio) BETWEEN ? AND ? OR 
+  def verificar_disponibilidad_turnos_fijos
+    TurnosFijo.where("(HOUR(hora_inicio) BETWEEN ? AND ? OR 
                         HOUR(hora_fin) BETWEEN ? AND ? ) AND 
                         dia_semana = ? AND  
                         cancha_id = ? AND 
@@ -33,5 +36,9 @@ class TurnosFijo < ActiveRecord::Base
                         cliente_id <> ? ", (hora_inicio + 1.seconds).strftime("%H:%M"), (hora_fin - 1.seconds).strftime("%H:%M"),
                                            (hora_inicio + 1.seconds).strftime("%H:%M"), (hora_fin - 1.seconds).strftime("%H:%M"),
                                            dia_semana, cancha.id, cliente.id  ).present?
- end
+  end
+  
+  def validar_horario
+    errors.add(:horario_incorrecto, "Verifique los horarios.") if hora_inicio.strftime("%H:%M") > hora_fin.strftime("%H:%M")
+  end
 end
